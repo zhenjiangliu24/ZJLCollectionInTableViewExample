487fc1522b059754d7f460c7e1a05fce85e0131b
lovehuahua24 <lovehuahua24@gmail.com>
2016-06-21 22:48:01 -0400

    Collection View Inside Table View
    In Swift

---
+++ ZJLCollectionInTableViewExample/ZJLCollectionInTableViewController/ZJLCollectionInTableViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  ZJLCollectionInTableViewCell.swift
+//  ZJLCollectionInTableViewExample
+//
+//  Created by ZhongZhongzhong on 16/6/21.
+//  Copyright © 2016年 ZhongZhongzhong. All rights reserved.
+//
+
+import UIKit
+
+class ZJLCollectionView: UICollectionView {
+    var indexPath : NSIndexPath!
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+let ZJLCollectionViewCellIdentifier : NSString = "myCollectionViewCell"
+
+class ZJLCollectionInTableViewCell: UITableViewCell {
+    var collectionView : ZJLCollectionView!
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        let layout : UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsetsMake(5, 5, 5, 5)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.itemSize = CGSizeMake(50, 50)
+        layout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        self.collectionView = ZJLCollectionView.init(frame: CGRectZero, collectionViewLayout: layout)
+        self.collectionView.registerClass(UICollectionView.self, forCellWithReuseIdentifier: ZJLCollectionViewCellIdentifier as String)
+        self.collectionView.backgroundColor = UIColor.whiteColor()
+        self.collectionView.showsHorizontalScrollIndicator = false
+        self.contentView.addSubview(self.collectionView)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let frame = self.contentView.bounds
+        self.collectionView.frame = CGRectMake(0, 0, frame.width, frame.height)
+    }
+    
+    func setCollectionViewDataSourceDelegate(dataSourceDelegate delegate: protocol<UICollectionViewDelegate,UICollectionViewDataSource>, index: NSInteger) {
+        self.collectionView.dataSource = delegate
+        self.collectionView.delegate = delegate
+        self.collectionView.tag = index
+        self.collectionView.reloadData()
+    }
+    
+    func setCollectionViewDataSourceDelegate(dataSourceDelegate delegate: protocol<UICollectionViewDelegate,UICollectionViewDataSource>, indexPath: NSIndexPath) {
+        self.collectionView.dataSource = delegate
+        self.collectionView.delegate = delegate
+        self.collectionView.indexPath = indexPath
+        self.collectionView.tag = indexPath.section
+        self.collectionView.reloadData()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ZJLCollectionInTableViewExample/ZJLCollectionInTableViewController/ZJLCollectionInTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ZJLCollectionInTableViewController.swift
+//  ZJLCollectionInTableViewExample
+//
+//  Created by ZhongZhongzhong on 16/6/21.
+//  Copyright © 2016年 ZhongZhongzhong. All rights reserved.
+//
+
+import UIKit
+
+let reuseTableCell : NSString = "myTableCell"
+let reuseCollectionCell : NSString = "myCollectionViewCell"
+
+
+class ZJLCollectionInTableViewController: UITableViewController {
+
+}
